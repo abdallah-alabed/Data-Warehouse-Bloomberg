62e7105359afa5b54506fc0de79f833833b62015
AbdallahAlabed <abdallah.alabed@alawnehpay.com>
2025-04-27 20:49:38 +0300

    Initial commit

---
+++ Data-Warehouse-bloomberg/src/main/java/Abdallah/Alabed/Data_Warehouse_bloomberg/DataWarehouseBloombergApplication.java
@@ -0,0 +1,13 @@
+package Abdallah.Alabed.Data_Warehouse_bloomberg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWarehouseBloombergApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataWarehouseBloombergApplication.class, args);
+	}
+
+}
---
+++ Data-Warehouse-bloomberg/src/test/java/Abdallah/Alabed/Data_Warehouse_bloomberg/DataWarehouseBloombergApplicationTests.java
@@ -0,0 +1,13 @@
+package Abdallah.Alabed.Data_Warehouse_bloomberg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWarehouseBloombergApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
